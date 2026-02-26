745160fd8496c0385e1c9732f11dd9d1227acb74
emanuelsantos953 <emanuelsantos953@gmail.com>
2026-02-26 18:41:01 +0000

    Adicionado modelo de Funcionario com matricula, nome, pis e cpf

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Funcionario(models.Model):
+    matricula = models.CharField(max_length=20, unique=True)
+    nome_completo = models.CharField(max_length=255)
+    pis = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.matricula} - {self.nome_completo}"
